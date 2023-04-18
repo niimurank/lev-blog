017b7210a0db6f1a5d66f005890f5f72a3f3f504
niimurank <nk2002826@gmail.com>
2023-04-18 20:30:26 +0900

    表示投稿の最新10件のみ表示

--- blog/resources/views/posts/index.blade.php
+++ blog/resources/views/posts/index.blade.php
@@ -7,8 +7,10 @@
     <h2 class="title">ブログの名前</h2>
     <div>
         <div>
-            <h4 class="header1">{{ $title-> title}}</h4>
-	    <p class="art1">{{ $title-> body}}</p>
+	    @foreach ($post as $post1)
+            <h4 class="header1">{{$post1->title}}</h4>
+	    <p class="art1">{{$post1->body}}</p>
+            @endforeach
         </div>
         <div>
             <h4 class="header2">見出し2</h4>
