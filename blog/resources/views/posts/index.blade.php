0767bcfacefaeae7fc7711b3e27e5889c65af401
niimurank <nk2002826@gmail.com>
2023-04-19 19:23:46 +0900

    idによる投稿の個別表示

--- blog/resources/views/posts/index.blade.php
+++ blog/resources/views/posts/index.blade.php
@@ -2,17 +2,24 @@
 <html>
 <head>
     <title>ブログの名前</title>
+    <link rel="stylesheet" href="/index.css">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <head>
 <body>
     <h2 class="title">ブログの名前</h2>
-    <div>
-        <div>
+    <div class="container">
 	    @foreach ($post as $post1)
-            <h4 class="header1">{{$post1->title}}</h4>
-	    <p class="art1">{{$post1->body}}</p>
+	    <div class="card m-5 p-2">
+            <h4 class="card-title">{{$post1->title}}</h4>
+	    <p class="card-text">{{$post1->body}}</p>
+	    </div>
 	    @endforeach
-	</div>
-        {{ $post->links() }}
+	    <div class="pagination">
+	    {{ $post->links() }}
+	    </div>
     </div>
 </body>
 </html>
